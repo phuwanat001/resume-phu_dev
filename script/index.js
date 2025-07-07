c5c4c2742958ac7b64dfd327dbbf60022a0c9ad7
phuwanatz_dev <phuwanat22294@gmail.com>
2025-07-08 02:34:56 +0700

    first commit

---
+++ script/index.js
@@ -0,0 +1,33 @@
+
+  const text = "I Am Into Web Developer"; // แก้ Devloper เป็น Developer
+  const speed = 100;
+  const eraseSpeed = 50;
+  const delay = 1000;
+  let i = 0;
+  let isDeleting = false;
+  const target = document.getElementById("typewriter");
+
+  function typeLoop() {
+    if (!isDeleting) {
+      target.innerHTML = text.substring(0, i + 1);
+      i++;
+      if (i === text.length) {
+        isDeleting = true;
+        setTimeout(typeLoop, delay);
+      } else {
+        setTimeout(typeLoop, speed);
+      }
+    } else {
+      target.innerHTML = text.substring(0, i - 1);
+      i--;
+      if (i === 0) {
+        isDeleting = false;
+        setTimeout(typeLoop, delay);
+      } else {
+        setTimeout(typeLoop, eraseSpeed);
+      }
+    }
+  }
+{}
+  typeLoop(); // เริ่มทำงาน
+
