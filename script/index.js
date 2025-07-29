cc75e7d163fda9b3376e0a6a8aa8b6fc5b5dfdda
phuwanatz_dev <phuwanat22294@gmail.com>
2025-07-29 21:34:23 +0700

    edit

--- script/index.js
+++ script/index.js
@@ -1,32 +1,11 @@
-const text = "I Am Into Web Developer"; // แก้ Devloper เป็น Developer
-const speed = 100;
-const eraseSpeed = 50;
-const delay = 1000;
-let i = 0;
-let isDeleting = false;
-const target = document.getElementById("typewriter");
+// document.querySelectorAll(".nav-link").forEach((link) => {
+//   link.addEventListener("click", () => {
+//     const navbarToggler = document.querySelector(".navbar-toggler");
+//     const navbarCollapse = document.querySelector(".navbar-collapse");
 
-function typeLoop() {
-  if (!isDeleting) {
-    target.innerHTML = text.substring(0, i + 1);
-    i++;
-    if (i === text.length) {
-      isDeleting = true;
-      setTimeout(typeLoop, delay);
-    } else {
-      setTimeout(typeLoop, speed);
-    }
-  } else {
-    target.innerHTML = text.substring(0, i - 1);
-    i--;
-    if (i === 0) {
-      isDeleting = false;
-      setTimeout(typeLoop, delay);
-    } else {
-      setTimeout(typeLoop, eraseSpeed);
-    }
-  }
-}
-{
-}
-typeLoop(); // เริ่มทำงาน
+   
+//     if (navbarCollapse.classList.contains("show") && window.innerWidth < 992) {
+//       navbarToggler.click();
+//     }
+//   });
+// });
